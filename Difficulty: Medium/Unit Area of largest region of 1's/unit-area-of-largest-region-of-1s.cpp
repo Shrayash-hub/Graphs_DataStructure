0ff4c189e74fd9c3aea564ad9efe3f13a92a1ef7
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-28 11:16:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unit Area of largest region of 1's/unit-area-of-largest-region-of-1s.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    int dfs(vector<vector<int>>& grid,int i,int j,int row,int clm){
+        if(i<0 || j<0 || i>=row || j>=clm){
+            return 0;
+        }
+        
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+
+        // mark visited
+        grid[i][j] = 0;
+        int area =1;
+        area += dfs(grid,i+1,j,row,clm);
+        area += dfs(grid,i-1,j,row,clm);
+        area += dfs(grid,i,j+1,row,clm);
+        area += dfs(grid,i,j-1,row,clm);
+        
+        area += dfs(grid,i+1,j+1,row,clm);
+        area += dfs(grid,i-1,j-1,row,clm);
+        area += dfs(grid,i+1,j-1,row,clm);
+        area += dfs(grid,i-1,j+1,row,clm);
+        
+        return area;
+    }
+    // Function to find unit area of the largest region of 1s.
+    int findMaxArea(vector<vector<int>>& grid) {
+        // Code here
+        if (grid.empty()) return 0;
+        int maxArea = 0;
+        int row = grid.size();
+        int clm = grid[0].size();
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                  int area = dfs(grid,i,j,row,clm) ;
+                  maxArea = max(area,maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+};
