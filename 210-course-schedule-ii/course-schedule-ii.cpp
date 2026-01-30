75723507f8c33b4f2e5163bbcbab76ab20628f8e
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-30 12:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (83.53%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(V);
+        vector<int> indegree(V,0);
+        for(int i=0;i<prerequisites.size();i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[u].push_back(v);
+            // indegree : number of incoming prerequisites
+            indegree[v]++;
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        vector<int> ans;
+        while(!q.empty()){
+            int temp = q.front();
+            ans.push_back(temp);
+            q.pop();
+            for(auto it: adj[temp]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+            
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()==V) return ans;
+        return {};
+    }
+};
