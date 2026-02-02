2abc31f8011c214889502109a1c03dc65da44612
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-02 16:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (11.07%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,13 +3,7 @@ public:
     int minimumOperations(vector<int>& nums) {
         int oper = 0;
         for (int i = 0; i < nums.size(); i++) {
-            int temp = nums[i] % 3;
-            if(temp<3-temp){
-                oper+= temp;
-            }
-            else{
-                oper+=(3-temp);
-            }
+            oper+= min(nums[i] % 3,3-(nums[i] % 3));
         }
         return oper;
     }
