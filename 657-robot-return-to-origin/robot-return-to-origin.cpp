92d59ff26ce1702e827859db5aee99f21a3b21ac
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-19 02:09:22 +0530

    Time: 1 ms (61.33%) | Memory: 10.4 MB (13.15%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int u = 0, d = 0, l = 0, r = 0;
+        for(int i = 0; i < moves.length(); i++){
+            if(moves[i] == 'U') u++;
+            else if(moves[i] == 'D') d++;
+            else if(moves[i] == 'L') l++;
+            else r++;
+        }
+        return l==r && u==d;
+    }
+};
