beb1993e0e052eed3b2e97e25e842b4103a25925
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-19 02:01:30 +0530

    Time: 4 ms (27.12%) | Memory: 20.9 MB (25.89%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -12,9 +12,11 @@ public:
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        // as matrix is given , we need to make adj list by ourself
         vector<int> adj[n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < isConnected[0].size(); j++) {
+                // if there exist a path between the cities i and j
                 if (i != j && isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
@@ -24,6 +26,8 @@ public:
         int cnt=0;
         vector<int> visited(n, 0);
         for (int i = 0; i < n; i++) {
+            // jitni bar ye condition run hogi utney hi provinces hongey
+            // kyuki hm ek province ki sari cities ko toh ek city k through cover kr lenge , but next province k liye , waps se dfs call krna padhega
             if (visited[i] == 0) {
                 cnt++;
                 dfs(i,adj,visited);
