506005b83726334f78dc219128e7aea89890cb6f
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-21 02:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (56.06%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
     void dfs(vector<vector<int>>& grid,int i,int j,int oldColor,int newColor){
         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
             return;
@@ -12,12 +14,13 @@ public:
     
         // Paint with new color
         grid[i][j] = newColor;
-    
+
         // Recursively fill in all 4 directions
-        dfs(grid, i+1, j, oldColor, newColor);
-        dfs(grid, i-1, j, oldColor, newColor);
-        dfs(grid, i, j+1, oldColor, newColor);
-        dfs(grid, i, j-1, oldColor, newColor);
+        for(int k=0;k<4;k++){
+            int newi = i + dx[k];
+            int newj = j + dy[k];
+            dfs(grid,newi,newj,oldColor,newColor);
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if (image.empty()) return image;
