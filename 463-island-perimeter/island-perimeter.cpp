8288345f20b20c9145a497a931f3034cfd2ad940
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-23 19:30:01 +0530

    Time: 12 ms (26.75%) | Memory: 102.2 MB (40.07%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, int i, int j, int row, int clm) {
-        // if water, no perimeter
-        if (grid[i][j] == 0) {
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+
+        // out of bounds -> contributes 1
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+            return 1;
+
+        // water -> contributes 1
+        if (grid[i][j] == 0)
+            return 1;
+
+        // already visited land -> contributes 0
+        if (grid[i][j] == -1)
             return 0;
+
+        // mark visited
+        grid[i][j] = -1;
+
+        int peri = 0;
+
+        for (int k = 0; k < 4; k++) {
+            peri += dfs(grid, i + dx[k], j + dy[k]);
         }
-
-        int peri = 4;
-
-        // up
-        if (i > 0 && grid[i-1][j] == 1) peri--;
-
-        // left
-        if (j > 0 && grid[i][j-1] == 1) peri--;
-
-        // right
-        if (j < clm-1 && grid[i][j+1] == 1) peri--;
-
-    // down
-        if (i < row-1 && grid[i+1][j] == 1) peri--;
 
         return peri;
     }
+
     int islandPerimeter(vector<vector<int>>& grid) {
-        int finalPeri = 0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                int peri = helper(grid,i,j,grid.size(),grid[0].size());
-                finalPeri+=peri;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j);
+                }
             }
         }
-        return finalPeri;
+
+        return 0;
     }
 };
