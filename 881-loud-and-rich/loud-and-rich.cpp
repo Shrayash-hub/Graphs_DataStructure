05c79ab9f013418a8ebfaea09f27f70fb25210b7
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-02-28 23:38:26 +0530

    Time: 11 ms (58.06%) | Memory: 47 MB (76.26%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.cpp
+++ 881-loud-and-rich/loud-and-rich.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
-
-    int dfs(int person,
-            vector<vector<int>>& graph,
-            const vector<int>& quiet,
-            vector<int>& memo) {
-
-        if (memo[person] != -1)
-            return memo[person];
-
-        int quietest = person;
-
-        for (int nei : graph[person]) {
-            int candidate = dfs(nei, graph, quiet, memo);
-
-            if (quiet[candidate] < quiet[quietest])
-                quietest = candidate;
-        }
-
-        memo[person] = quietest;
-        return quietest;
-    }
-
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
 
         int n = quiet.size();
-        vector<vector<int>> graph(n);
 
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+
+        // build graph: richer -> poorer
         for (auto &r : richer) {
-            int a = r[0];
-            int b = r[1];
-            graph[b].push_back(a);
+            int a = r[0]; // richer
+            int b = r[1]; // poorer
+
+            graph[a].push_back(b);
+            indegree[b]++;
         }
 
-        vector<int> memo(n, -1);
+        // initially each person is their own quietest
         vector<int> ans(n);
-
         for (int i = 0; i < n; i++)
-            ans[i] = dfs(i, graph, quiet, memo);
+            ans[i] = i;
+
+        // kahn's algorithm
+        queue<int> q;
+
+        // start with nodes having indegree 0 (no one richer)
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : graph[u]) {
+
+                // propagate quietest
+                if (quiet[ans[u]] < quiet[ans[v]]) {
+                    ans[v] = ans[u];
+                }
+
+                indegree[v]--;
+
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
+        }
 
         return ans;
     }
