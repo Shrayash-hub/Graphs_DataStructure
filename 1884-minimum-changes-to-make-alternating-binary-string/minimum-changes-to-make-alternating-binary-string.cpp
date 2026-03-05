a7811552eedd27f2e70949cfcd1973fef200c1c0
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-03-05 13:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (20.96%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] - '0' != i % 2) {
+                count++;
+            }
+        }
+        int mini = min(count,(int)s.size() - count);
+        return mini;
+    }
+};
