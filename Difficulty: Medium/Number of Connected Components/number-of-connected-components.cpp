a2f7d6ce9c51ded55dce4160a36049a53db114c8
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-07-02 20:12:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
+++ Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
@@ -1,35 +1,32 @@
 class Solution {
-  public:
-    void dfs(vector<int> adj[], int node, vector<int> &vis){
-        vis[node] = 1;
-        
-        for(auto it: adj[node]){
-            if(vis[it]==0){
-                dfs(adj,it,vis);
-            }
-        }
-    }
-    int countConnected(int V, vector<vector<int>>& edges) {
-        // code here
-        int count = 0;
-        vector<int> adj[V];
-        
-        for(auto edge: edges){
-            int u = edge[0];
-            int v = edge[1];
-            
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        
-        vector<int> vis(V,0);
-        
-        for(int i = 0; i<V; i++){
-            if(vis[i]==0){
-                count++;
-                dfs(adj,i,vis);
-            }
-        }
-        return count;
-    }
+	public:
+	void dfs(vector<int> adj[], vector<bool>& visited, int node) {
+		visited[node] = true;
+		
+		for (auto it: adj[node]) {
+			if (visited[it] == false)
+				dfs(adj, visited, it);
+		}
+	}
+	int countConnected(int V, vector<vector<int>> & edges) {
+		// code here
+		vector<int> adj[V];
+		for (auto it: edges) {
+			int u = it[0];
+			int v = it[1];
+			
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		
+		vector<bool> visited(V, false);
+		int count = 0;
+		for (int i = 0; i < V; i++) {
+			if (visited[i] != true) {
+				count++;
+				dfs(adj, visited, i);
+			}
+		}
+		return count;
+	}
 };
